c48614ac19d151074048fbb0dba0e2b160c22a30
Daniel Jost <info@daniel-jost.de>
2016-07-14 21:01:42 +0200

    added migration for role static data

---
+++ app/DoctrineMigrations/Version20160714185804.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20160714185804 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('INSERT INTO roles (id, name, role) VALUES (NULL, \'User\', \'ROLE_USER\')');
+        $this->addSql('INSERT INTO roles (id, name, role) VALUES (NULL, \'Administrator\', \'ROLE_ADMIN\')');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DELETE FROM roles WHERE role IN (\'ROLE_USER\', \'ROLE_ADMIN\')');
+    }
+}
